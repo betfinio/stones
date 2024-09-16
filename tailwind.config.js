f63a535de822f87614c9f8563e511da4cf99693a
bfmonster <bf.monster@proton.me>
2024-09-16 11:57:12 +0200

    fixed build?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	darkMode: ['class'],
 	important: '.stones',
-	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}', './@mf-types/**/*.*'],
+	content: ['./src/**/*.{ts,tsx}'],
 	prefix: '',
 	theme: {
 		container: {
