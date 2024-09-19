e59dafdd2d8e95cd0faf32fff1ca908feda757a4
bfmonster <bf.monster@proton.me>
2024-09-19 11:12:48 +0200

    removed unused vars

--- src/components/TableBet/TableBet.tsx
+++ src/components/TableBet/TableBet.tsx
@@ -1,34 +1,6 @@
 import AllRoundsTable from '@/src/components/TableBet/AllRoundsTable.tsx';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from 'betfinio_app/tabs';
 import { useTranslation } from 'react-i18next';
-import crystal1 from '../../assets/Roulette/crystal1.svg';
-import crystal2 from '../../assets/Roulette/crystal2.svg';
-import crystal3 from '../../assets/Roulette/crystal3.svg';
-import crystal4 from '../../assets/Roulette/crystal4.svg';
-import crystal5 from '../../assets/Roulette/crystal5.svg';
-
-interface TableRow {
-	date: string;
-	round: string;
-	sum: string;
-	stone: string;
-	winning: string;
-	bonus: string;
-	transactionId: string;
-}
-
-interface MockTableData {
-	myBets: TableRow[];
-	allRounds: TableRow[];
-}
-
-const images: { [key: string]: string } = {
-	crystal1,
-	crystal2,
-	crystal3,
-	crystal4,
-	crystal5,
-};
 
 const TableBet = () => {
 	const { t } = useTranslation('', { keyPrefix: 'stones.table' });
