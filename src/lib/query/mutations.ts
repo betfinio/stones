64b38666a29a13935ebeb624384cebc626264f81
bf-tower <bf.tower@proton.me>
2025-06-30 19:22:01 +0300

    fix: unify all error toasts to use handleError function in stones

--- src/lib/query/mutations.ts
+++ src/lib/query/mutations.ts
@@ -2,7 +2,7 @@ import logger from '@/src/config/logger';
 import { type DistributeParams, type PlaceBetParams, type SpinParams, distribute, placeBet, spin } from '@/src/lib/api';
 import { toast } from '@betfinio/components/ui';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { getTransactionLink } from 'betfinio_context/lib/helpers';
+import { getTransactionLink, handleError } from 'betfinio_context/lib/helpers';
 import { useTranslation } from 'react-i18next';
 import type { WriteContractErrorType, WriteContractReturnType } from 'viem';
 import { getTransaction, waitForTransactionReceipt } from 'viem/actions';
@@ -37,19 +37,13 @@ export const usePlaceBet = () => {
 			queryClient.invalidateQueries({ queryKey: ['stones'] });
 		},
 		onError: (error) => {
+			console.dir(error);
 			//@ts-ignore
 			const errorData = JSON.parse(JSON.stringify(error.cause));
-			if (errorData.reason) {
-				toast.error(tErrors('default'), {
-					description: tErrors(errorData.reason, { defaultValue: tLocalError(errorData.reason) }),
-				});
-			} else if (errorData.signature) {
-				toast.error(tErrors('default'), {
-					description: tErrors(errorData.signature, { defaultValue: tLocalError(errorData.signature) }),
-				});
-			} else {
-				toast.error(tErrors('unknown'));
-			}
+
+			if (errorData.reason) toast.error(handleError(errorData.reason, tErrors, tLocalError));
+			else if (errorData.signature) toast.error(handleError(errorData.signature, tErrors, tLocalError));
+			else toast.error(tErrors('unknown'));
 		},
 		onMutate: () => {
 			logger.start('placing bet');
